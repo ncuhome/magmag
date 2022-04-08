005599c84e6a3d55c5c1f3e4b24ec12fa8d0a871
Kuss <kussssss@outlook.com>
2022-04-08 16:33:38 +0800

    feat: toastify

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,30 @@
-export const THROTTLE_TIME = 120
+import Toastify from 'toastify-js'
 
 export const SMALL_COUNTS = 100
+
+export const enum ToastType {
+  JOIN,
+  QUIT
+}
+
+const toastTypeStyleMap = {
+  [ToastType.JOIN]: {
+    boxShadow: '0 3px 6px -1px rgba(0, 0, 0, 0.12), 0 10px 36px -4px rgba(255, 130, 58, 0.3)',
+    background: '#FF823A'
+  },
+  [ToastType.QUIT]: {
+    boxShadow: '0 3px 6px -1px rgba(0, 0, 0, 0.12), 0 10px 36px -4px rgba(240, 65, 85, 0.3)',
+    background: '#F04155'
+  }
+}
+
+export const toast = (text: string, type: ToastType) => {
+  Toastify({
+    text,
+    duration: 3000,
+    gravity: 'top',
+    position: 'center',
+    style: toastTypeStyleMap[type]
+  })
+    .showToast()
+}
