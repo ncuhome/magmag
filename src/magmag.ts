ee5891161f4c74a57a202297564f1ee6e167b38c
Kuss <kussssss@outlook.com>
2022-04-13 23:33:50 +0800

    fix: magmag -> syncStates deduplicate

--- src/magmag.ts
+++ src/magmag.ts
@@ -245,10 +245,12 @@ export class Magmag {
     if (map.size > 1) {
       const othersLen = map.size - 1
       if (othersLen > this.playerObjs.length) {
-        const toAdd = (Array.from(map.values()) as PlayerState[])
+        const othersId = (Array.from(map.values()) as PlayerState[])
           .filter(state => state.id && state.id !== awareness.clientID)
           .map(state => state.id)
-        if (toAdd.length > 0) {
+        if (othersId.length > 0) {
+          const currentIds = this.playerObjs.map(playerObj => playerObj.id)
+          const toAdd = othersId.filter(id => !currentIds.includes(id))
           state.added.push(...toAdd)
           state.added = Array.from(new Set(state.added))
         }
@@ -269,7 +271,9 @@ export class Magmag {
     state.added.forEach(id => {
       const otherPlayer = map.get(id)
       if (otherPlayer?.pos?.x) {
-        const newBody = this.createBall(otherPlayer.pos.x, otherPlayer.pos.y, otherPlayer.uid, 0.6)
+        const newBody = this.createBall(otherPlayer.pos.x, otherPlayer.pos.y, otherPlayer.uid, {
+          opacity: 0.6
+        })
         this.playerObjs.push({
           id: otherPlayer.id,
           body: newBody
@@ -303,7 +307,9 @@ export class Magmag {
   }
 
   private addPlayer = () => {
-    this.player = this.createBall(this.width / 2, this.height / 2, this.uid)
+    this.player = this.createBall(this.width / 2, this.height / 2, this.uid, {
+      attractorEnabled: true
+    })
 
     World.add(this.world, [
       this.player
@@ -357,20 +363,20 @@ export class Magmag {
     World.add(this.world, [top, bottom])
   }
 
-  private createBall = (x: number, y: number, id: string, opacity = 1.0, radius = 40) => {
+  private createBall = (x: number, y: number, id: string, {
+    attractorEnabled = false,
+    opacity = 1.0,
+    radius = 40
+  }: {
+    attractorEnabled?: boolean
+    opacity?: number
+    radius?: number
+  }) => {
     const throttleRecover = throttle((body: Body) => {
       bodyScale(body, 0.01)
     }, 200)
-    return Bodies.circle(x, y, radius, {
-      render: {
-        // @ts-ignore
-        sprite: {
-          texture: `data:image/svg+xml;utf8,${generateFromString(id)}`
-        },
-        opacity
-      },
-      plugin: {
-        attractors: [
+    const attractors = attractorEnabled
+      ? [
           (bodyA: Body, bodyB: Body) => {
             if (this.world) {
               if (SAT.collides(bodyA, bodyB).collided) {
@@ -394,11 +400,19 @@ export class Magmag {
               x: (bodyA.position.x - bodyB.position.x) * (1e-7) * scale * extraFactor,
               y: (bodyA.position.y - bodyB.position.y) * (1e-7) * scale * extraFactor
             }
-
-            // Body.applyForce(bodyA, bodyA.position, Matter.Vector.neg(force))
-            // Body.applyForce(bodyB, bodyB.position, force)
           }
         ]
+      : null
+    return Bodies.circle(x, y, radius, {
+      render: {
+        // @ts-ignore
+        sprite: {
+          texture: `data:image/svg+xml;utf8,${generateFromString(id)}`
+        },
+        opacity
+      },
+      plugin: {
+        attractors
       }
     })
   }
